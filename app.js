eabe1c85561548d19885a20a66d5a4c0b8e46009
Mario Torres <mario291187@gmail.com>
2023-06-26 18:04:18 -0600

    dpanel.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var dpanelRouter = require('./routes/dpanel');
 var apiRouter = require('./routes/api');
 
 var app = express();
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/dpanel', dpanelRouter);
 app.use('/api', apiRouter);
 
 module.exports = app;
